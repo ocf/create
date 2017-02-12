0ad4b0e0ce030e25b4e726b1171af833d80cc5d1
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2017-02-12 15:24:53 -0800

    Add healthcheck to discover current workers and latency
    Currently this can be run manually by cloning it and running "python -m
    create.healthcheck", but eventually I want to run one instance of this
    on Marathon with it logging to syslog, and maybe add alerting when the
    number drops below 3 workers or something.

---
+++ create/healthcheck.py
@@ -0,0 +1,75 @@
+"""Check the number of create workers."""
+import argparse
+import configparser
+import ssl
+import time
+
+from celery import Celery
+from ocflib.account import submission
+
+
+def discover_workers(tasks, n):
+    workers = set()
+    latencies = []
+
+    for i in range(n):
+        start = time.time()
+
+        task = tasks.status.delay()
+        result = task.wait(timeout=5)
+        workers.add(result['host'])
+
+        latencies.append((time.time() - start) * 1000)
+
+    return workers, latencies
+
+
+def celery_app():
+    # TODO: reduce duplication with create.tasks
+    conf = configparser.ConfigParser()
+    conf.read('/etc/ocf-create/ocf-create.conf')
+
+    celery = Celery(
+        broker=conf.get('celery', 'broker'),
+        backend=conf.get('celery', 'backend'),
+    )
+    # TODO: use ssl verification
+    celery.conf.broker_use_ssl = {
+        'ssl_cert_reqs': ssl.CERT_NONE,
+    }
+    # `redis_backend_use_ssl` is an OCF patch which was proposed upstream:
+    # https://github.com/celery/celery/pull/3831
+    celery.conf.redis_backend_use_ssl = {
+        'ssl_cert_reqs': ssl.CERT_NONE,
+    }
+
+    # TODO: stop using pickle
+    celery.conf.task_serializer = 'pickle'
+    celery.conf.result_serializer = 'pickle'
+    celery.conf.accept_content = {'pickle'}
+
+    return celery
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '--num-checks', '-n',
+        type=int, default=100,
+        help='number of status checks to run',
+    )
+    args = parser.parse_args(argv)
+
+    tasks = submission.get_tasks(celery_app())
+    workers, latencies = discover_workers(tasks, args.num_checks)
+
+    print('{} workers: {}'.format(len(workers), sorted(workers)))
+    print('latencies: mean: {:.02f}ms; min: {:.02}ms; max: {:.02f}ms'.format(
+        sum(latencies) / len(latencies),
+        min(latencies),
+        max(latencies),
+    ))
+
+
+if __name__ == '__main__':
+    exit(main())
